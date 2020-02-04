91845af9d8b8ddc85e3b577dbd5707f2d50e9447
jorge.cordoba <jorge.cs@gmail.com>
2020-02-04 10:02:15 +0100

    Fixed errors on constructors due to refactor

--- microservices/src/main/kotlin/com/devfields/xmf/common/microservices/microservices/Response.kt
+++ microservices/src/main/kotlin/com/devfields/xmf/common/microservices/microservices/Response.kt
@@ -1,7 +1,13 @@
 package com.devfields.xmf.common.microservices.microservices
 
+import com.devfields.xmf.common.messaging.MessageContext
+import com.devfields.xmf.common.types.Version
+
 /**
  * Created by jco27 on 02/01/2017.
  */
-abstract class Response(val requestId: String) : Command(){
+abstract class Response(val requestId: String,
+                        serviceName: String,
+                        version : Version,
+                        context: MessageContext) : Command(serviceName, version, context){
 }
